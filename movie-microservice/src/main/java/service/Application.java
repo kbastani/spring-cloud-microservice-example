dc0f6f6e4ff6bc2e23b30ae1d4ceb3e1a65c739e
Kenny Bastani <kb@socialmoon.com>
2015-08-16 19:25:23 -0700

    Enabling Spring Cloud Features

--- movie-microservice/src/main/java/service/Application.java
+++ movie-microservice/src/main/java/service/Application.java
@@ -4,6 +4,7 @@ import data.domain.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
@@ -12,6 +13,7 @@ import org.springframework.hateoas.hal.Jackson2HalModule;
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = { "config", "data" })
 public class Application {
     public static void main(String[] args) {
